b82384bb67761d49a47c4ab713704eeae3c1b53f
rania-rgb <gleerania@gmail.com>
2024-12-17 12:07:59 +0100

    Update getaddrinfo.h

--- getaddrinfo.h
+++ getaddrinfo.h
@@ -1,13 +1,16 @@
-#ifndef TFTP_UTILS_H
-#define TFTP_UTILS_H
+#ifndef GETADDRINFO_H
+#define GETADDRINFO_H
 
 #include <sys/socket.h>
 #include <netdb.h>
 #include <stdlib.h>
-// Fonction pour obtenir l'adresse du serveur à partir d'une IP et d'un port
-struct addrinfo *get_server_address(char *server_ip, char *port);
+#include <stdio.h>
+
+// Fonction pour obtenir l'adresse IP et le port via getaddrinfo
+struct addrinfo *get_server_address(const char *server_ip, const char *port);
 
 // Fonction pour créer un socket UDP
 int create_socket();
 
-#endif /* TFTP_UTILS_H */
+#endif /* GETADDRINFO_H */
+
